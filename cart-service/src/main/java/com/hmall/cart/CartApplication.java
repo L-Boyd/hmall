c1f38cd599f98a182a16fec49d2439540f0cdcc0
lby-magicbook <1508201988@qq.com>
2025-07-19 16:42:56 +0800

    refactor:将远程调用客户端独立为一个module

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableFeignClients //开启openFeign
+@EnableFeignClients(basePackages = "com.hmall.api.client") //开启openFeign
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 public class CartApplication {
