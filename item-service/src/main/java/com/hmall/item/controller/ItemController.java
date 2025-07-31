decc21bdef4d3549e9c8c5938f969d92f632ae8a
lby_rog <1508201988@qq.com>
2025-07-31 22:08:08 +0800

    feat:测试filter

--- item-service/src/main/java/com/hmall/item/controller/ItemController.java
+++ item-service/src/main/java/com/hmall/item/controller/ItemController.java
@@ -26,7 +26,9 @@ public class ItemController {
 
     @ApiOperation("分页查询商品")
     @GetMapping("/page")
-    public PageDTO<ItemDTO> queryItemByPage(PageQuery query) {
+    public PageDTO<ItemDTO> queryItemByPage(PageQuery query, @RequestHeader(value = "truth", required = false) String truth, @RequestHeader("default-filters-k") String dfkey) {
+        System.out.println(truth);
+        System.out.println(dfkey);
         // 1.分页查询
         Page<Item> result = itemService.page(query.toMpPage("update_time", false));
         // 2.封装并返回
