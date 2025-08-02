bd06741c1a2be039ae1680778be426fe337a7470
lby_rog <1508201988@qq.com>
2025-08-02 23:35:24 +0800

    feat:测试GlobalFilter

--- hm-api/src/main/java/com/hmall/api/client/UserClient.java
+++ hm-api/src/main/java/com/hmall/api/client/UserClient.java
@@ -1,11 +1,19 @@
 package com.hmall.api.client;
 
+import com.hmall.api.dto.LoginFormDTO;
+import com.hmall.api.vo.UserLoginVO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("user-service")
 public interface UserClient {
+
+    @PostMapping("/users/login")
+    UserLoginVO login(@RequestBody @Validated LoginFormDTO loginFormDTO);
 
     @PutMapping("/users/money/deduct")
     void deductMoney(@RequestParam("pw") String pw, @RequestParam("amount") Integer amount);
