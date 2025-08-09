997071f7c45b6a5d32b9c4c6e890525d19ece570
lby_rog <1508201988@qq.com>
2025-08-09 14:52:35 +0800

    feat:无参GatewayFilter

---
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/PrintAnyGatewayFilterFactory.java
@@ -0,0 +1,26 @@
+package com.hmall.gateway.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+// 固定结尾：GatewayFilterFactory方便配置,Object处写参数类型
+public class PrintAnyGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return new OrderedGatewayFilter(new GatewayFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                System.out.println("print any filter running");
+                return chain.filter(exchange);
+            }
+        }, -1);
+    }
+
+}
