356a0a889ed6dd44b548b7de4ebcf71601aadfd2
lby_rog <1508201988@qq.com>
2025-08-22 15:05:11 +0800

    feat:用代码基于注解完成交换机、队列的创建和绑定

--- mq-demo/consumer/src/main/java/com/itheima/consumer/mq/SpringRabbitListener.java
+++ mq-demo/consumer/src/main/java/com/itheima/consumer/mq/SpringRabbitListener.java
@@ -1,6 +1,10 @@
 package com.itheima.consumer.mq;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -37,12 +41,20 @@ public class SpringRabbitListener {
         log.info("消费者2收到消息：{}", message);
     }
 
-    @RabbitListener(queues = "direct.queue1")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue1", declare = "true"),
+            exchange = @Exchange(name = "hmall.direct", type = ExchangeTypes.DIRECT),
+            key = {"red", "blue"}
+    ))
     public void listenDirectQueue1(String message) {
         log.info("消费者1收到direct.queue1的消息：{}", message);
     }
 
-    @RabbitListener(queues = "direct.queue2")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue2", declare = "true"),
+            exchange = @Exchange(name = "hmall.direct", type = ExchangeTypes.DIRECT),
+            key = {"red", "yellow"}
+    ))
     public void listenDirectQueue2(String message) {
         log.info("消费者2收到direct.queue2的消息：{}", message);
     }
