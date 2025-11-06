49d3db49b5eb02e8a4c814578c4c158ea03552af
lby-magicbook <1508201988@qq.com>
2025-11-06 17:25:45 +0800

    feat：测试es新建索引

--- item-service/src/test/java/com/hmall/item/es/ElasticSearchTest.java
+++ item-service/src/test/java/com/hmall/item/es/ElasticSearchTest.java
@@ -1,8 +1,12 @@
 package com.hmall.item.es;
 
+import net.sf.jsqlparser.statement.create.index.CreateIndex;
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +16,50 @@ import java.io.IOException;
 public class ElasticSearchTest {
 
     private RestHighLevelClient client;
+
+    // 在kibana写的创建索引模板，直接复制过来的
+    private static final String CREATE_INDEX_TEMPLATE = "{\n" +
+            "  \"mappings\": {\n" +
+            "    \"properties\": {\n" +
+            "      \"id\": {\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"name\":{\n" +
+            "        \"type\": \"text\",\n" +
+            "        \"analyzer\": \"ik_max_word\"\n" +
+            "      },\n" +
+            "      \"price\":{\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"stock\":{\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"image\":{\n" +
+            "        \"type\": \"keyword\",\n" +
+            "        \"index\": false\n" +
+            "      },\n" +
+            "      \"category\":{\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"brand\":{\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"sold\":{\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"commentCount\":{\n" +
+            "        \"type\": \"integer\",\n" +
+            "        \"index\": false\n" +
+            "      },\n" +
+            "      \"isAD\":{\n" +
+            "        \"type\": \"boolean\"\n" +
+            "      },\n" +
+            "      \"updateTime\":{\n" +
+            "        \"type\": \"date\"\n" +
+            "      }\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
 
     @BeforeEach
     void setUp() {
@@ -30,5 +78,15 @@ public class ElasticSearchTest {
     @Test
     void testConnection() {
         System.out.println("client = " + client);
+    }
+
+    @Test
+    void testCreateIndex() throws IOException {
+        // Request对象
+        CreateIndexRequest request = new CreateIndexRequest("items"); // "items" 索引名称
+        // 请求参数
+        request.source(CREATE_INDEX_TEMPLATE, XContentType.JSON);
+        // 发送请求
+        client.indices().create(request, RequestOptions.DEFAULT);
     }
 }
